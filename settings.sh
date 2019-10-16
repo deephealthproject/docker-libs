8729dae990fd69fe3c42883638ebd8d72872c75c
simleo <simleo@crs4.it>
2019-10-16 11:06:39 +0200

    fix eddl compilation on gpu

--- settings.sh
+++ settings.sh
@@ -14,17 +14,17 @@ LATEST_BRANCH=master
 
 # software repositories
 ECVL_BRANCH=master
-EDDL_BRANCH=master
+EDDL_BRANCH=develop
 PYECVL_BRANCH=master
 PYEDDL_BRANCH=master
 ECVL_REVISION=3b07fabc005c3fca2c7e38cce6502a2d62b61010
-EDDL_REVISION=3823f150b22df401f2e7a39bb104454e444ea890
+EDDL_REVISION=160de37d300f8b0d7e5f8fddc36c920acf728048
 PYECVL_REVISION=791a044127092e31c52617be1ebacb97b9107092
 PYEDDL_REVISION=4afab146b38b9de8c89171a16904340e121d6499
-ECVL_REPOSITORY=https://github.com/deephealthproject/ecvl.git
-EDDL_REPOSITORY=https://github.com/deephealthproject/eddl.git
-PYECVL_REPOSITORY=https://github.com/deephealthproject/pyecvl.git
-PYEDDL_REPOSITORY=https://github.com/deephealthproject/pyeddl.git
+ECVL_REPOSITORY=git@github.com:deephealthproject/ecvl.git
+EDDL_REPOSITORY=git@github.com:deephealthproject/eddl.git
+PYECVL_REPOSITORY=git@github.com:deephealthproject/pyecvl.git
+PYEDDL_REPOSITORY=git@github.com:deephealthproject/pyeddl.git
 
 # date.time as build number
 #BUILD_NUMBER ?= $(shell date '+%Y%m%d.%H%M%S')
