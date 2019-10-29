b0822d74c37f47496411693eac0b223d0af128a0
Marco Enrico Piras <kikkomep@crs4.it>
2019-10-29 11:33:50 +0100

    Update software revision

--- settings.sh
+++ settings.sh
@@ -15,22 +15,23 @@ LATEST_BRANCH=master
 # ECVL repository
 ECVL_REPOSITORY=git@github.com:deephealthproject/ecvl.git
 ECVL_BRANCH=master
-ECVL_REVISION=3b07fabc005c3fca2c7e38cce6502a2d62b61010
+ECVL_REVISION=ce069064bb49442fc07ba34ed2f66dc8f1ababc1
 
 # PyECVL
 PYECVL_REPOSITORY=git@github.com:deephealthproject/pyecvl.git
 PYECVL_BRANCH=master
-PYECVL_REVISION=791a044127092e31c52617be1ebacb97b9107092
+PYECVL_REVISION=ec5357a4274b8561d254a6a750b6eeba404100b4
 
 # EDDL repository 
 EDDL_REPOSITORY=git@github.com:deephealthproject/eddl.git
 EDDL_BRANCH=develop
-EDDL_REVISION=160de37d300f8b0d7e5f8fddc36c920acf728048
+EDDL_REVISION=a2e44cdd7ad99ae16aa686aeb72085998cc24557
 
 # PyEDDL repository
 PYEDDL_REPOSITORY=git@github.com:deephealthproject/pyeddl.git
 PYEDDL_BRANCH=master
-PYEDDL_REVISION=4afab146b38b9de8c89171a16904340e121d6499
+PYEDDL_REVISION=f0e7e2c9d0ecbfe065a187465102cf77b6c2fcd6
 
 # date.time as build number
 #BUILD_NUMBER ?= $(shell date '+%Y%m%d.%H%M%S')
+BUILD_NUMBER = 0.1
