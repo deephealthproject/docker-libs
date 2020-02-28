829c647f9a11787b374481d20b33c7c0d5c02265
kikkomep <kikkomep@crs4.it>
2020-02-28 11:18:39 +0000

    Fix missing log function

--- tests/test_pyeddl.sh
+++ tests/test_pyeddl.sh
@@ -11,22 +11,22 @@ gpus=$(docker run --runtime=nvidia nvidia/cuda:10.0-base nvidia-smi -L 2>&-)
 
 cd ${PYEDDL_SRC}/examples
 if [ -z ${gpus} ]; then
-    log 'INFO: No GPU available. Running tests with no GPU....'
-    log 'Downloading test dataset'
+    echo 'INFO: No GPU available. Running tests with no GPU....'
+    echo 'Downloading test dataset'
     wget -q  https://www.dropbox.com/s/khrb3th2z6owd9t/trX.bin
 	wget -q https://www.dropbox.com/s/m82hmmrg46kcugp/trY.bin
 	wget -q https://www.dropbox.com/s/7psutd4m4wna2d5/tsX.bin
 	wget -q https://www.dropbox.com/s/q0tnbjvaenb4tjs/tsY.bin
-	log 'Running tests....'
+	echo 'Running tests....'
     pytest tests
-    log 'Running examples....'
+    echo 'Running examples....'
 	python3 Tensor/array_tensor_save.py
 	python3 NN/1_MNIST/mnist_auto_encoder.py --epochs 1
 else
-    log "Available GPUs: \n ${gpus}"
-    log "Running tests..."
+    echo "Available GPUs: \n ${gpus}"
+    echo "Running tests..."
     python3 Tensor/array_tensor_save.py
-    log 'Running examples....'
+    echo 'Running examples....'
     bash NN/1_MNIST/run_all_fast.sh
     bash NN/py_loss_metric/run_all_fast.sh
     bash onnx/run_all_fast.sh
