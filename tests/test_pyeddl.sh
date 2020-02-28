fdba65dc737cc1a828ebe909f04bb219b99d1cfc
kikkomep <kikkomep@crs4.it>
2020-02-28 15:14:52 +0000

    Use GPU_RUNTIME as flag to choose testing set

--- tests/test_pyeddl.sh
+++ tests/test_pyeddl.sh
@@ -6,27 +6,21 @@ PYEDDL_SRC=${PYEDDL_SRC:-"/usr/local/src/pyeddl"}
 # run tests
 cd ${PYEDDL_SRC} && pytest tests 
 
-# check GPU availability
-gpu_check=".gpus_check"
-docker run --runtime=nvidia nvidia/cuda:10.0-base nvidia-smi -L > ${gpu_check} 2>&-
-
 # run examples
 cd ${PYEDDL_SRC}/examples
-if [ -s ${gpu_check} ]; then
-    echo 'INFO: No GPU available. Running tests with no GPU....'
+if [ -z "${GPU_RUNTIME}" ]; then
     echo 'Downloading test dataset'
     wget -q  https://www.dropbox.com/s/khrb3th2z6owd9t/trX.bin
     wget -q https://www.dropbox.com/s/m82hmmrg46kcugp/trY.bin
     wget -q https://www.dropbox.com/s/7psutd4m4wna2d5/tsX.bin
     wget -q https://www.dropbox.com/s/q0tnbjvaenb4tjs/tsY.bin
-    echo 'Running tests....'
+    echo 'Running tests for CPUs....'
     pytest tests
     echo 'Running examples....'
     python3 Tensor/array_tensor_save.py
     python3 NN/1_MNIST/mnist_auto_encoder.py --epochs 1
 else
-    echo "Available GPUs: \n ${gpus}"
-    echo "Running tests..."
+    echo "Running tests for GPUs..."
     python3 Tensor/array_tensor_save.py
     echo 'Running examples....'
     bash NN/1_MNIST/run_all_fast.sh
