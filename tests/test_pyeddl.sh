21c9b552d16cce921caa991d129c961f868ca5f4
kikkomep <kikkomep@crs4.it>
2020-02-28 11:28:19 +0000

    Fix indentation

--- tests/test_pyeddl.sh
+++ tests/test_pyeddl.sh
@@ -14,14 +14,14 @@ if [ -z ${gpus} ]; then
     echo 'INFO: No GPU available. Running tests with no GPU....'
     echo 'Downloading test dataset'
     wget -q  https://www.dropbox.com/s/khrb3th2z6owd9t/trX.bin
-	wget -q https://www.dropbox.com/s/m82hmmrg46kcugp/trY.bin
-	wget -q https://www.dropbox.com/s/7psutd4m4wna2d5/tsX.bin
-	wget -q https://www.dropbox.com/s/q0tnbjvaenb4tjs/tsY.bin
-	echo 'Running tests....'
+    wget -q https://www.dropbox.com/s/m82hmmrg46kcugp/trY.bin
+    wget -q https://www.dropbox.com/s/7psutd4m4wna2d5/tsX.bin
+    wget -q https://www.dropbox.com/s/q0tnbjvaenb4tjs/tsY.bin
+    echo 'Running tests....'
     pytest tests
     echo 'Running examples....'
-	python3 Tensor/array_tensor_save.py
-	python3 NN/1_MNIST/mnist_auto_encoder.py --epochs 1
+    python3 Tensor/array_tensor_save.py
+    python3 NN/1_MNIST/mnist_auto_encoder.py --epochs 1
 else
     echo "Available GPUs: \n ${gpus}"
     echo "Running tests..."
