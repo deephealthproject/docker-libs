523c3257e61318c2af0d308dae1a27154f04e08f
Marco Enrico Piras <kikkomep@crs4.it>
2020-04-22 18:02:46 +0000

    Update tests

--- tests/test_pyecvl.sh
+++ tests/test_pyecvl.sh
@@ -12,9 +12,4 @@ cd ${PYECVL_SRC} && pytest tests
 
 # run examples
 cd ${PYECVL_SRC}/examples 
-python3 dataset.py "${ECVL_SRC}/examples/data/mnist/mnist.yml"
-python3 ecvl_eddl.py "${ECVL_SRC}/examples/data/test.jpg" "${ECVL_SRC}/examples/data/mnist/mnist.yml"
-python3 img_format.py "${ECVL_SRC}/examples/data/nifti/LR_nifti.nii" "${ECVL_SRC}/examples/data/dicom/ISIC_0000008.dcm"
-python3 imgproc.py "${ECVL_SRC}/examples/data/test.jpg"
-python3 openslide.py "${ECVL_SRC}/examples/data/hamamatsu/test3-DAPI 2 (387).ndpi"
-python3 read_write.py "${ECVL_SRC}/examples/data/test.jpg" "test_mod.jpg"
+bash run_all.sh "${ECVL_SRC}/examples/data"
