aaa1f3a5588f556351b8ed3b6316a56e6b357092
Marco Enrico Piras <kikkomep@crs4.it>
2020-04-27 14:14:28 +0000

    Disable alias '_build'

--- jenkins.sh
+++ jenkins.sh
@@ -170,7 +170,7 @@ function run() {
 
   # define Docker image tags
   export BUILD_NUMBER=${BUILD_NUMBER:-$(date '+%Y%m%d%H%M%S')}
-  export DOCKER_IMAGE_TAG_EXTRA="${DOCKER_IMAGE_PREFIX}_build${BUILD_NUMBER}"
+  #export DOCKER_IMAGE_TAG_EXTRA="${DOCKER_IMAGE_PREFIX}_build${BUILD_NUMBER}"
   #export DOCKER_IMAGE_TAG_EXTRA="${DOCKER_IMAGE_TAG_EXTRA} ${NORMALIZED_BRANCH_NAME}_build${BUILD_NUMBER}"
 
   # set branch
