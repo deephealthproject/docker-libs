01140a3149b619eadbb000be0f3841cb56c0a96e
Luca Pireddu <pireddu@crs4.it>
2020-05-21 18:26:33 +0200

    Script for scratch image with nvidia ENV

---
+++ gen-base-runtime.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+function usage_error() {
+  if [[ $# -ge 1 ]]; then
+    echo -e "${@}"
+  fi
+  printf "Usage: %s NVIDIA_IMG\n" "$0"
+  exit 2
+}
+
+####### main #######
+
+if [[ $# != 1 ]]; then
+  usage_error
+fi
+
+NvidiaImage="${1}"
+
+printf "Copying nvidia environment from ${NvidiaImage}\n" >&2
+
+printf "FROM scratch\n"
+# We copy the entire environment except for TERM and HOSTNAME
+docker run -it --rm --entrypoint /usr/bin/env "${NvidiaImage}" | \
+  sed -n -e '/^TERM=/d; /^HOSTNAME=/d; s/^/ENV /p;'
+
