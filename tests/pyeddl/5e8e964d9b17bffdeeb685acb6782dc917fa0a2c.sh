cb5a09749e8fa39e4352e78e06685cd2950c5328
simleo <simleo@crs4.it>
2020-10-16 11:50:41 +0200

    pyeddl tests: add mnist examples

--- tests/pyeddl/5e8e964d9b17bffdeeb685acb6782dc917fa0a2c.sh
+++ tests/pyeddl/5e8e964d9b17bffdeeb685acb6782dc917fa0a2c.sh
@@ -15,7 +15,19 @@ pytest tests
 if [ -z "${GPU_RUNTIME}" ]; then
     echo 'Running CPU examples'
     python3 examples/Tensor/array_tensor_save.py
+    wget -q https://www.dropbox.com/s/khrb3th2z6owd9t/mnist_trX.bin
+    wget -q https://www.dropbox.com/s/m82hmmrg46kcugp/mnist_trY.bin
+    wget -q https://www.dropbox.com/s/7psutd4m4wna2d5/mnist_tsX.bin
+    wget -q https://www.dropbox.com/s/q0tnbjvaenb4tjs/mnist_tsY.bin
+    python3 examples/NN/1_MNIST/mnist_auto_encoder.py --epochs 1 --small
+    rm -fv mnist_*.bin
 else
     echo 'Running GPU examples'
     python3 examples/Tensor/array_tensor_save.py
+    wget -q https://www.dropbox.com/s/khrb3th2z6owd9t/mnist_trX.bin
+    wget -q https://www.dropbox.com/s/m82hmmrg46kcugp/mnist_trY.bin
+    wget -q https://www.dropbox.com/s/7psutd4m4wna2d5/mnist_tsX.bin
+    wget -q https://www.dropbox.com/s/q0tnbjvaenb4tjs/mnist_tsY.bin
+    bash examples/NN/1_MNIST/run_all_fast.sh
+    rm -fv mnist_*.bin
 fi
